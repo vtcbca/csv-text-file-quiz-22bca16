c73697dda06503e88ea5840f8c004d2a52b33530
22bca16 <122865076+22bca16@users.noreply.github.com>
2023-08-21 11:13:07 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -4,13 +4,13 @@ import csv
 with open('salary.csv','w',newline='')as f:
     w.csv.writer(f)
     header=['sid','sname','salary']
-    w.writerow(c)
+    w.writerow(header)
     record=[[1,'om',4500],
            [2,'sai',4200],
            [3,'ram',5000],
            [4,'radha',5500],
            [5,'krishna',5700]]
-w.writerows(d)
+w.writerows(record)
 e=open('salary.csv','r')
 f=csv.reader(e)
 for i in f:
